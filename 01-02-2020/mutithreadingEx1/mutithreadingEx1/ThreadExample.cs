53f9a847fb62c6bcfb387afbbec9fa04dcb30924
abhinav <abhinavsujina@gmail.com>
2020-02-01 14:20:50 +0530

    updated

---
+++ 01-02-2020/mutithreadingEx1/mutithreadingEx1/ThreadExample.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mutithreading
+{
+    class ThreadExample
+    {
+        static void Main(string[] args)
+        {
+            ThreadExample th = new ThreadExample();
+            th.CreateThread();
+
+        }
+        private void CreateThread()
+        {
+            Thread maths = new Thread(mythread);
+            maths.Name = "thread1";
+            maths.Start();
+            Console.WriteLine("main thread ends here");
+            Console.ReadKey();
+
+        }
+        
+        private void  mythread()
+        {
+            Console.WriteLine("status of the thred: "+Thread.CurrentThread.IsAlive);
+            Console.WriteLine("name of the current thread is :" + Thread.CurrentThread.Name);
+            Console.WriteLine("thread sleeps for 3 sec");
+            Thread.Sleep(3000);
+            Console.WriteLine("thread is back");
+            Console.WriteLine("the state of the thread is : " + Thread.CurrentThread.ThreadState);
+            Console.WriteLine("abort the thread");
+            Thread.CurrentThread.Abort();
+
+
+
+        }
+    }
+}
---
+++ 01-02-2020/two_threads/two_threads/Multi.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace two_threads
+{
+    class Mythread
+    {
+        public void Mythread1()
+        {
+            for(int i=0;i<10;i++)
+            {
+                Console.WriteLine(i+" name :"+Thread.CurrentThread.Name); 
+            }
+        }
+        public void mythread2()
+        {
+            for(int j=0;j<4;j++)
+            {
+                Console.WriteLine("this lines will execute respectively");
+            }
+        }
+    }
+
+    class Multi
+    {
+        static void Main(string[] args)
+        {
+            Multi mt = new Multi();
+            mt.createthread();
+        }
+        public void  createthread()
+        {
+            Mythread mt = new Mythread();
+            Thread thread4 = new Thread(new ThreadStart(mt.Mythread1));
+            Thread thread1 = new Thread(new ThreadStart(mt.Mythread1));
+            Thread thread2 = new Thread(new ThreadStart(mt.Mythread1));
+            Thread thread3 = new Thread(new ThreadStart(mt.mythread2));
+            Console.WriteLine("thread joining example");
+            thread1.Name = "first";
+            thread2.Name = "second";
+            thread4.Name = "Prior thread";
+            thread4.Priority = ThreadPriority.Highest;
+            thread4.Start();
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+            thread3.Join();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
