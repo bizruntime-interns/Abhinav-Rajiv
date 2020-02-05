90530f6fa872da2841f9ce1ee469b9ba80023518
abhinav <abhinavsujina@gmail.com>
2020-02-05 18:06:50 +0530

    updated

---
+++ 05-02-2020(c#)/hashedset/hashedset/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hashedset
+{
+    class hashedset
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> hash = new HashSet<int>();
+            for(int i=0;i<5; i++)
+            {
+                hash.Add(i);
+            }
+            if(hash.Contains(1))
+            {
+               
+                Console.WriteLine("true");
+            }
+            HashSet<int> set3 = new HashSet<int>();
+            for (int i = 0; i < 5; i++)
+            {
+               set3.Add(i);
+            }
+            Console.WriteLine(hash.Equals(set3));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 05-02-2020(c#)/multicast_dele/multicast_dele/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+delegate int operations(int n);
+namespace multicast_dele
+{
+    class Deleegates
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        static int num = 10;
+        public static int Add(int a)
+        {
+            num += a;
+            return num;
+        }
+        public static int mul(int b)
+        {
+            num += b;
+            return num;
+        }
+        public static int getnum()
+        {
+            return num;
+        }
+        static void Main(string[] args)
+        {
+            operations op;
+            operations op1 = new operations(Add);
+            operations op2 = new operations(mul);
+            op = op1;
+            op += op2;
+            op(20);
+            Console.WriteLine(getnum());
+            int outt = getnum();
+            log.Error("num is ");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
