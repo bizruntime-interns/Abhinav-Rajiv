8eceb00757040c97ecb9103ba1d9ab3a01424335
abhinav <abhinavsujina@gmail.com>
2020-02-13 10:28:57 +0530

    updated

---
+++ 11-02-2020(c#)/linq/linq/Employees.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq
+{
+    class Employees
+    {
+        public String name
+        {
+            get;
+            set;
+        }
+        public int id
+        {
+            get;
+            set;
+        }
+        public string desi
+        {
+            get;
+            set;
+        }
+        public int salary
+        {
+            get;
+            set;
+        }
+    }
+}
