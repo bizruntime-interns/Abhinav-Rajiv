f34b90b70d81af5c77589e7df8ccd816698ae73e
abhinav <abhinavsujina@gmail.com>
2020-02-22 15:20:22 +0530

    updated

---
+++ 17-02-2020(c#delegates)/Eventargs/Eventargs/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace Eventargs
+{
+    class Events
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {          
+             Counter c = new Counter();
+            c.ThresholdReached += C_ThresholdReached;
+            log.Debug("enter a");
+            while (Console.ReadKey(true).KeyChar == 'a')
+            {
+                log.Info("enter again");
+                c.Count();
+            }
+            Console.ReadKey();
+        }
+
+        private static void C_ThresholdReached(object sender, Thresholdevent e)
+        {
+            log.Info("the count has been exeeded " + e.count);           
+        }
+    }
+    class Counter
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public delegate void counted(object obj,EventArgs args);
+
+        int count = 0;
+        public void Count()
+        {
+            count++;
+            log.Info(count);
+            if(count>5)
+            {
+                Thresholdevent theve = new Thresholdevent();
+                theve.count = count;
+                theve.date = DateTime.Now;
+                OnThresholdReached(theve);
+            }
+        }
+
+        protected virtual void OnThresholdReached(Thresholdevent e)
+        {
+            EventHandler<Thresholdevent> handler = ThresholdReached;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        public event EventHandler<Thresholdevent> ThresholdReached;
+    }
+    class Thresholdevent : EventArgs
+    {
+        public int count { get; set; }
+        public DateTime date { get; set; }
+    }
+
+}
+    
+
---
+++ 17-02-2020(c#delegates)/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace videoecoder
+{
+    public class Videoencoder
+    {
+        public delegate void Videoencode(object obj, EventArgs args);
+        public Videoencode OnvideoEncoded;
+        public void Encode()
+        {
+            Console.WriteLine("VIDEO IS ENCODING");
+            Thread.Sleep(3000);
+            OnEncoded();
+        }
+        protected virtual void  OnEncoded()
+        {
+            if(OnvideoEncoded!=null)
+            {
+                OnvideoEncoded(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    class Message
+    {
+        public void messagesend(object obj,EventArgs args)
+        {
+            Console.WriteLine("message is send");
+        }
+    }
+
+    class Video
+    {
+        static void Main(string[] args)
+        {
+            Videoencoder ve = new Videoencoder();
+            Message mes = new Message();
+            AudioEncoder audio = new AudioEncoder();
+
+            ve.OnvideoEncoded += mes.messagesend;
+            audio.Audioenc = mes.messagesend;
+
+            ve.Encode();
+            audio.AudioEncode();
+
+            Console.ReadKey();
+        }
+    }
+
+    class AudioEncoder
+    {
+        public delegate void AudioEncoded(object obj,EventArgs args);
+        public AudioEncoded Audioenc;
+        public void AudioEncode()
+        {
+            Console.WriteLine("Audio Encoding");
+            Thread.Sleep(3000);
+            onEncode();
+        }
+        public void onEncode()
+        {
+            Audioenc?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
