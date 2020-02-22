066e0433401cc34ecad5804366d8d5e10edf272e
abhinav <abhinavsujina@gmail.com>
2020-02-22 15:23:05 +0530

    update

---
+++ 20-02-2020(c#)/xmlCsharp/xmlCsharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Threading.Tasks;
+
+namespace xmlCsharp
+{
+    class Students
+    {
+        static void Main(string[] args)
+        {
+            XmlReader reader = XmlReader.Create("books.xml");
+            while (reader.Read())
+            {
+                if()
+            }
+        }
+    }
+}
