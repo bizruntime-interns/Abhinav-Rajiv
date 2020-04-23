cb45c572b1159422492dd43745023b6fca4e4080
abhinav <abhinavsujina@gmail.com>
2020-04-23 21:47:17 +0530

    updated

---
+++ reactieNetFirst/reactieNetFirst/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reactive.Linq;
+using System.Reactive;
+
+using System.Threading.Tasks;
+
+namespace reactieNetFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var observable = Observable.Range(5, 10);
+
+            IObserver<int> observer = Observer.Create<int>(
+                Console.WriteLine, (error) => { Console.WriteLine($"error:{error.Message}"); },
+                () => { Console.WriteLine("observer completed"); });
+
+
+            var sub = observable.Subscribe(observer);
+            Console.ReadKey();
+
+            sub.Dispose();
+        }
+    }
+}
