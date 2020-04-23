8228a071765676dafe79d96b875e4c0e20a95247
abhinav <abhinavsujina@gmail.com>
2020-04-23 21:55:44 +0530

    updated

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                IObservable<int> source = Observable.Generate(
+                    5, i => i < 10,
+                    i => i + 1,
+                    i => i * i,
+                    i=>TimeSpan.FromSeconds(i)
+                    //i => TimeSpan.FromSeconds(i)
+                );
+
+                using (source.Subscribe(
+                    x => Console.WriteLine("OnNext:  {0}", x),
+                    ex => Console.WriteLine("OnError: {0}", ex.Message),
+                    () => Console.WriteLine("OnCompleted")
+                ))
+                {
+                    Console.WriteLine("Press ENTER to unsubscribe...");
+                    Console.ReadLine();
+                }
+            
+        }  
+    }
+}
