4d750e70ed643efcf96d3f7458b3e34bc0ca0086
abhinav <abhinavsujina@gmail.com>
2020-04-23 21:25:53 +0530

    updated

---
+++ igniteDemo/igniteDemo/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace igniteDemo
+{
+    class Person
+    {
+        public String  Name { get; set; }
+        public int Age{ get; set; }
+        public override string ToString()
+        {
+            return $"Person [name={Name} ,Age= {Age}]";
+        }
+    }
+}
---
+++ igniteDemo/igniteDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apache.Ignite.Core;
+using Apache.Ignite.Core.Binary;
+using Apache.Ignite.Core.Cache;
+
+namespace igniteDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //cache Demo 
+
+            //IIgnite ignt= Ignition.Start();
+            // ICache<string, string> cache = ignt.GetOrCreateCache<string, string>("test");
+            // cache.Put("name", "abhinav rajiv");
+            // Console.Clear();
+            // Console.WriteLine(cache.Get("name"));
+            // if (cache.PutIfAbsent("salary", "10000000"))
+            //     Console.WriteLine("salary added to cache..\n");
+            // else
+            //     Console.WriteLine(cache.Get("salary"));
+
+
+            var cfng = new IgniteConfiguration
+            {
+                BinaryConfiguration = new BinaryConfiguration(typeof(Person))
+            };
+            IIgnite ing = Ignition.Start();
+            ICache<int, Person> cache = ing.GetOrCreateCache<int, Person>("personc");
+            cache[1] = new Person { Name = "Abhinav", Age = 21 };
+            cache[2] = new Person { Name = "Amal", Age = 13 };
+            Console.Clear();
+            foreach(ICacheEntry<int,Person> entry in cache)
+            {
+                Console.WriteLine(entry);
+            }
+            Console.ReadKey();
+        }
+    }
+}
