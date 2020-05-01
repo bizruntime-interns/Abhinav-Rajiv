c60c621ba24bcd1c888cfa61a1a7ba2d16266e71
abhinav <abhinavsujina@gmail.com>
2020-05-01 06:16:15 +0530

    pulsar exaple

---
+++ firstappPulsar/firstapp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Pulsar.Client.Api;
+
+namespace firstapp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            run();
+           
+            // await TlsAuthentication.RunTlsAuthentication();
+            // await CustomProps.RunCustomProps();
+            // await ReaderApi.RunReader();
+            // await RealWorld.RunRealWorld(PulsarClient.Logger);
+
+            Console.WriteLine("Example ended. Press any key to exit");
+            
+            Console.ReadKey();
+
+
+        }
+        public static async void run()
+        {
+            await message.startpulsar();
+        }
+    }
+}
---
+++ firstappPulsar/firstapp/message.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pulsar.Client.Api;
+
+namespace firstapp
+{
+    class message
+    {
+        public static async Task startpulsar()
+        {
+            Console.WriteLine("enterred");
+            const string serviceurl = "pulsar://localhost:30002";
+            const string subname = "mysub";
+            var topicname = $"mytopic";
+
+            var client = new PulsarClientBuilder().ServiceUrl(serviceurl)
+                .Build();
+
+            var producer = await new ProducerBuilder(client)
+                .Topic(topicname)
+                .CreateAsync();
+
+            var consumer = await new ConsumerBuilder(client)
+                .Topic(topicname)
+                .SubscriptionName(subname)
+                .SubscribeAsync();
+
+            var messageid = await producer.SendAsync(Encoding.UTF8.GetBytes($"send from abhi at '{DateTime.Now}'"));
+            Console.WriteLine($"message '{messageid}'");
+
+            var message = await consumer.ReceiveAsync();
+            Console.WriteLine($"received : {Encoding.UTF8.GetString(message.Data)}");
+
+            await consumer.AcknowledgeAsync(message.MessageId);
+        }
+
+    }
+}
